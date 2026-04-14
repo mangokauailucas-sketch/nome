279617fd346383f7348a1e5a484c54976604bc27
mangokauailucas-sketch <mangokauailucas@gmail.com>
2026-04-14 16:06:28 -0300

    Delete dist directory

--- dist/server/index.js
+++
@@ -1,9 +0,0 @@
-import { $, a0 } from "./assets/worker-entry-DdTh0iFd.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream/web";
-import "node:stream";
-export {
-  $ as createServerEntry,
-  a0 as default
-};
